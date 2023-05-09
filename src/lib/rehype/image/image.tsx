1701b675d18d552cde5d79a461228ca1a098c8bf
sor4chi <sorakawa0218@gmail.com>
2023-05-09 11:40:52 +0900

    feat: article内のimageをクリックするとmodalが開く用のcomponentを作成

---
+++ src/lib/rehype/image/image.tsx
@@ -0,0 +1,55 @@
+"use client";
+import NextImage from "next/image";
+import type { ComponentProps } from "react";
+import { useEffect, useState } from "react";
+
+import styles from "./image.module.css";
+
+import { useClickOutside } from "@/hooks";
+
+type Props = ComponentProps<"img">;
+
+export const Image = (props: Props) => {
+  const { alt, src } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const ref = useClickOutside(() => {
+    setIsExpanded(false);
+  });
+
+  useEffect(() => {
+    if (isExpanded) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isExpanded]);
+
+  const img = (
+    <span className={styles.imageContainer}>
+      <NextImage
+        alt={alt || ""}
+        src={src || ""}
+        fill
+        className={styles.image}
+        onClick={() => setIsExpanded(true)}
+      />
+    </span>
+  );
+
+  if (!isExpanded) {
+    return img;
+  }
+
+  return (
+    <>
+      {img}
+      <span className={styles.expanded}>
+        <span ref={ref}>
+          {/* eslint-disable @next/next/no-img-element */}
+          <img src={src} alt={alt} className={styles.expandedImage} />
+        </span>
+      </span>
+    </>
+  );
+};

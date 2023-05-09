21134ee9c03c9ab61127cab40a4ac913e344aa90
sor4chi <sorakawa0218@gmail.com>
2023-05-09 11:39:51 +0900

    feat(dep): remark-extract-tocがdtsをバンドルしていないので外部からアンビエント宣言する

---
+++ src/types/remark-extract-toc.d.ts
@@ -0,0 +1,14 @@
+declare module "remark-extract-toc" {
+  /**
+   * Type definition for the `extractToc` function.
+   * @param {Object} opt - Options object.
+   * @param {boolean} [opt.flatten=false] - Whether to flatten the TOC into a list of objects.
+   * @param {string[]} [opt.keys=[]] - Optional keys to include in each TOC object.
+   * @returns {function} A transformer function that extracts a table of contents from a given AST.
+   */
+  function extractToc(opt?: {
+    flatten?: boolean;
+    keys?: string[];
+  }): (ast: any, file: any) => any;
+  export = extractToc;
+}

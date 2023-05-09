cba54fd04ef460d26698bc30d65ef92ec23c3b67
sor4chi <sorakawa0218@gmail.com>
2023-05-09 22:36:01 +0900

    feat: tag-list componentを作成

---
+++ src/components/blog/tag-list/tag-list.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./tag-list.module.css";
+
+interface Props {
+  tags: string[];
+}
+
+const BLOG_LIST_BY_TAG_URL = (tag: string) => `/tag/${tag}`;
+
+export const TagList = ({ tags }: Props) => {
+  return (
+    <ul className={styles.tagList}>
+      {tags.map((tag) => (
+        <li key={tag} className={styles.tagItem}>
+          <Link className={styles.tag} href={BLOG_LIST_BY_TAG_URL(tag)}>
+            {tag}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};

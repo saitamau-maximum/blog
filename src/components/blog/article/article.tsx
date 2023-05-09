11baf68d1fdeb11ab04dbab8ce8fb1a6f4d77d18
sor4chi <sorakawa0218@gmail.com>
2023-05-10 02:50:51 +0900

    feat: レンダリング部分をarticle componentとして集約

---
+++ src/components/blog/article/article.tsx
@@ -0,0 +1,14 @@
+import { parseHTMLToReactJSX } from "@/lib/markdown";
+import styles from "./article.module.css";
+import "./prism.css";
+import "katex/dist/katex.min.css";
+
+interface Props {
+  content: string;
+}
+
+export const Article = ({ content }: Props) => {
+  return (
+    <article className={styles.content}>{parseHTMLToReactJSX(content)}</article>
+  );
+};

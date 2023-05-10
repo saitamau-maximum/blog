4ee5e3cc8bb04a4e71ab527be98b32269091d342
sor4chi <sorakawa0218@gmail.com>
2023-05-10 12:12:45 +0900

    feat: Heroのpropsからtitle setするように拡張

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,13 +1,19 @@
+"use client";
 import { ReactNode } from "react";
 import styles from "./hero.module.css";
+import { useHeader } from "@/hooks";
 
 interface Props {
+  title?: string;
   children: ReactNode;
 }
 
-export const Hero = ({ children }: Props) => {
+export const Hero = ({ children, title }: Props) => {
+  const { setTitle } = useHeader();
+  title && setTitle(title);
+
   return (
-    <div className={styles.hero}>
+    <div className={styles.hero} id="hero-area">
       <div className={styles.heroBg} />
       {children}
     </div>

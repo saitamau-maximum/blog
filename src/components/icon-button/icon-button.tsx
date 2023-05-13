18e1631c925edfe791e5c4795dc80d09715fc108
sor4chi <sorakawa0218@gmail.com>
2023-05-13 16:54:43 +0900

    feat(components): アイコンボタンを追加

---
+++ src/components/icon-button/icon-button.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps, ReactNode } from "react";
+import styles from "./icon-button.module.css";
+
+type Props = {
+  children: ReactNode;
+  onClick: () => void;
+  ariaLabel: string;
+} & ComponentProps<"button">;
+
+export const IconButton = ({ children, onClick, ariaLabel }: Props) => {
+  return (
+    <button
+      className={styles.iconButton}
+      onClick={onClick}
+      aria-label={ariaLabel}
+    >
+      {children}
+    </button>
+  );
+};

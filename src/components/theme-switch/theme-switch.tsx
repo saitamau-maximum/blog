5e1b6f4551db4fd18ae6581b93b64567421a7839
sor4chi <sorakawa0218@gmail.com>
2023-05-13 16:55:05 +0900

    refactor(theme-switch): テーマ切り替えボタンをIconButtonに変更

--- src/components/theme-switch/theme-switch.tsx
+++ src/components/theme-switch/theme-switch.tsx
@@ -1,57 +1,53 @@
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaMoon, FaSun } from "react-icons/fa";
 
-import styles from './theme-switch.module.css';
+import styles from "./theme-switch.module.css";
+import { IconButton } from "../icon-button";
 
-type Theme = 'light' | 'dark';
+type Theme = "light" | "dark";
 
 const judgeTheme = (): Theme => {
-  if (typeof window === 'undefined') {
-    return 'light';
+  if (typeof window === "undefined") {
+    return "light";
   }
-  if (localStorage.getItem('theme') === 'dark') {
-    return 'dark';
+  if (localStorage.getItem("theme") === "dark") {
+    return "dark";
   }
-  return 'light';
+  return "light";
 };
 
 export const ThemeSwitch = () => {
   const switchTheme = () => {
     const before = judgeTheme();
-    const tweets = document.querySelectorAll('[data-tweet-id]');
+    const tweets = document.querySelectorAll("[data-tweet-id]");
 
-    if (before === 'light') {
-      localStorage.setItem('theme', 'dark');
-      document.documentElement.classList.add('dark');
-      document.documentElement.classList.remove('light');
+    if (before === "light") {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
       tweets.forEach((tweet) => {
-        const src = tweet.getAttribute('src');
+        const src = tweet.getAttribute("src");
         if (src) {
-          tweet.setAttribute('src', src.replace('theme=light', 'theme=dark'));
+          tweet.setAttribute("src", src.replace("theme=light", "theme=dark"));
         }
       });
     }
-    if (before === 'dark') {
-      localStorage.setItem('theme', 'light');
-      document.documentElement.classList.add('light');
-      document.documentElement.classList.remove('dark');
+    if (before === "dark") {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.add("light");
+      document.documentElement.classList.remove("dark");
       tweets.forEach((tweet) => {
-        const src = tweet.getAttribute('src');
+        const src = tweet.getAttribute("src");
         if (src) {
-          tweet.setAttribute('src', src.replace('theme=dark', 'theme=light'));
+          tweet.setAttribute("src", src.replace("theme=dark", "theme=light"));
         }
       });
     }
   };
 
   return (
-    <button
-      className={styles.themeSwitch}
-      onClick={switchTheme}
-      id="theme-switch-button"
-      aria-label="theme switch button"
-    >
+    <IconButton onClick={switchTheme} ariaLabel="テーマを切り替える">
       <FaMoon className={styles.moon} />
       <FaSun className={styles.sun} />
-    </button>
+    </IconButton>
   );
 };

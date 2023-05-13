e14f8a0e35c62f4104a63f519f46c77cad9fad80
sor4chi <sorakawa0218@gmail.com>
2023-05-13 16:55:48 +0900

    feat(blog): ボタンリストコンポーネントを追加し、GitHubアイコンとテーマ切り替えボタンを表示

---
+++ src/components/blog/button-list/button-list.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { IconButton } from "@/components/icon-button";
+import { ThemeSwitch } from "@/components/theme-switch";
+import { FaGithub } from "react-icons/fa";
+import styles from "./button-list.module.css";
+
+interface Props {
+  repositoryUrl: string;
+}
+
+export const BlogButtonList = ({ repositoryUrl }: Props) => {
+  return (
+    <div className={styles.buttonList}>
+      <IconButton
+        onClick={() => {
+          window.open(repositoryUrl, "_blank");
+        }}
+        ariaLabel="記事のソースへ"
+      >
+        <FaGithub className={styles.githubIcon} />
+      </IconButton>
+      <ThemeSwitch />
+    </div>
+  );
+};
---
+++ src/constants/url.ts
@@ -0,0 +1,4 @@
+export const URL = {
+  GITHUB_REPOSITORY_BLOG_URL: (slug: string) =>
+    `https://github.com/saitamau-maximum/blog/blob/main/blog/${slug}.md`,
+};

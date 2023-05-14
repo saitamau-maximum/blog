e4a66c07ccb01679d07864e48e0ef435428d2251
sor4chi <sorakawa0218@gmail.com>
2023-05-14 22:10:54 +0900

    feat(blog-card): ブログカードコンポーネントを作成

---
+++ src/components/blog/card/card.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+import { URL } from "@/constants/url";
+
+interface Props {
+  title: string;
+  slug: string;
+  date: string;
+  authors: string[];
+  tags: string[];
+}
+
+export const BlogCard = ({ title, slug, date, authors, tags }: Props) => {
+  return (
+    <div className={styles.card}>
+      <Link href={`/blog/${slug}`} className={styles.cardLink}>
+        <div className={styles.cardHeader}>
+          <time className={styles.cardDate}>{date}</time>
+          <div className={styles.cardAuthors}>
+            {authors.map((author) => (
+              <Image
+                key={author}
+                src={URL.GITHUB_PROFILE_IMAGE_URL(author)}
+                alt={author}
+                width={32}
+                height={32}
+                className={styles.cardAuthorImage}
+              />
+            ))}
+          </div>
+        </div>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <div className={styles.cardFooter}>
+          <ul className={styles.cardTags}>
+            {tags.map((tag) => (
+              <li key={tag} className={styles.cardTagItem}>
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Link>
+    </div>
+  );
+};

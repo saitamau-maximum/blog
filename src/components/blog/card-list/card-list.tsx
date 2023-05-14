8dfe73ec897bd6cd884edd6065afffb728d9ee37
sor4chi <sorakawa0218@gmail.com>
2023-05-14 22:12:04 +0900

    refactor(components): ブログカードリストを実装

---
+++ src/components/blog/card-list/card-list.tsx
@@ -0,0 +1,38 @@
+import styles from "./card-list.module.css";
+import Link from "next/link";
+import { BlogCard } from "../card";
+
+interface Props {
+  blogs: {
+    title: string;
+    slug: string;
+    date: string;
+    authors: string[];
+    tags: string[];
+  }[];
+}
+
+export const BlogCardList = ({ blogs }: Props) => {
+  if (blogs.length === 0) {
+    return (
+      <div className={styles.cardListEmpty}>
+        <p className={styles.cardListEmptyMessage}>記事がありません。</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.cardList}>
+      {blogs.map((blog) => (
+        <BlogCard
+          key={blog.slug}
+          title={blog.title}
+          slug={blog.slug}
+          date={blog.date}
+          authors={blog.authors}
+          tags={blog.tags}
+        />
+      ))}
+    </div>
+  );
+};

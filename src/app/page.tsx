efba798e1f5eeb54f933d5708d3b39d4a71d1022
sor4chi <sorakawa0218@gmail.com>
2023-05-14 22:37:26 +0900

    feat(page): 新着ブログ記事一覧を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,42 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { TEXT } from "@/constants/text";
 import { Hero } from "../components/hero";
+import { readFile, readdir } from "fs/promises";
+import { URL } from "@/constants/url";
+import { parseStrToMarkdown } from "@/lib/markdown";
+import { BlogCardList } from "@/components/blog/card-list";
+import Link from "next/link";
+import { MdArrowForward } from "react-icons/md";
 
-export default function Home() {
+const LATEST_BLOGS_COUNT = 6;
+
+async function getLatestBlogs() {
+  const files = await readdir(URL.BLOG_DIR_PATH);
+  const blogs = await Promise.all(
+    files.map(async (file) => {
+      const filePath = URL.BLOG_FILE_PATH(file);
+      const content = await readFile(filePath, "utf-8");
+      const res = parseStrToMarkdown(content, filePath);
+      if (!res) return null;
+      return {
+        ...res.frontmatter,
+        slug: file.replace(/\.md$/, ""),
+      };
+    })
+  );
+  const filteredBlogs = blogs.filter(
+    (blog): blog is NonNullable<typeof blog> => blog !== null
+  );
+  const sortedBlogs = filteredBlogs.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  return sortedBlogs.slice(0, LATEST_BLOGS_COUNT);
+}
+
+export default async function Home() {
+  const latestBlogs = await getLatestBlogs();
+
   return (
     <>
       <Hero>
@@ -19,7 +53,12 @@ export default function Home() {
         </div>
       </Hero>
       <div className={styles.container}>
-        <p className={styles.sectionTitle}>Coming Soon...</p>
+        <h2>新着記事</h2>
+        <BlogCardList blogs={latestBlogs} />
+        <Link href="/blog" className={styles.moreLink}>
+          More
+          <MdArrowForward />
+        </Link>
       </div>
     </>
   );

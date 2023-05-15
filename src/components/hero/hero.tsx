54b5635403f1006115f4f49561e65ec347897131
sor4chi <sorakawa0218@gmail.com>
2023-05-15 14:26:36 +0900

    feat(hero): ページタイトルとパンくずリストを追加

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -2,13 +2,25 @@
 import { ReactNode, useEffect } from "react";
 import styles from "./hero.module.css";
 import { useHeader } from "@/hooks";
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
+import { clsx } from "clsx";
+import { FaSlash, FaSquareRootAlt } from "react-icons/fa";
+import { MdHome } from "react-icons/md";
+
+type BreadcrumbItem = {
+  title: string;
+  href: string;
+};
 
 interface Props {
   title?: string;
-  children: ReactNode;
+  children?: ReactNode;
+  information?: ReactNode;
+  breadcrumbs: BreadcrumbItem[];
 }
 
-export const Hero = ({ children, title }: Props) => {
+export const Hero = ({ children, information, title, breadcrumbs }: Props) => {
   const { setTitle, heroAreaRef } = useHeader();
   title && setTitle(title);
 
@@ -19,9 +31,43 @@ export const Hero = ({ children, title }: Props) => {
   }, [setTitle]);
 
   return (
-    <div className={styles.hero} ref={heroAreaRef}>
-      <div className={styles.heroBg} />
-      {children}
-    </div>
+    <>
+      <div className={styles.hero} ref={heroAreaRef}>
+        <div className={styles.heroBg} />
+        {children}
+      </div>
+      <div className={styles.heroOuter}>
+        <div className={styles.breadcrumbs}>
+          <MdHome size={20} className={styles.breadcrumbItem} />
+          {breadcrumbs.map((breadcrumb, index) => (
+            <>
+              {index !== breadcrumbs.length - 1 ? (
+                <Link
+                  href={breadcrumb.href}
+                  key={`${breadcrumb.title}-${index}-breadcrumb`}
+                  className={clsx(styles.breadcrumbItem, styles.link)}
+                >
+                  {breadcrumb.title}
+                </Link>
+              ) : (
+                <span
+                  key={`${breadcrumb.title}-${index}-breadcrumb`}
+                  className={styles.breadcrumbItem}
+                >
+                  {breadcrumb.title}
+                </span>
+              )}
+              {index !== breadcrumbs.length - 1 && (
+                <IoIosArrowForward
+                  className={styles.breadcrumbItem}
+                  key={`${breadcrumb.title}-${index}-arrow`}
+                />
+              )}
+            </>
+          ))}
+        </div>
+        {information}
+      </div>
+    </>
   );
 };

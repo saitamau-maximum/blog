15fc9699d8bb0e301277b68dcecdff910637f1d4
sor4chi <sorakawa0218@gmail.com>
2023-05-15 13:13:44 +0900

    fix: anchorとtagの折り返し位置とサイズ調整

--- src/lib/rehype/anchor/anchor.tsx
+++ src/lib/rehype/anchor/anchor.tsx
@@ -1,29 +1,42 @@
-import Link from 'next/link';
-import type { ComponentProps } from 'react';
+import Link from "next/link";
+import type { ComponentProps } from "react";
+import styles from "./anchor.module.css";
+import { clsx } from "clsx";
 
 type Props = ComponentProps<typeof Link> & {
   href: string;
 };
 
 export const Anchor = ({ children, href, ...rest }: Props) => {
-  if (href.startsWith('/')) {
+  if (href.startsWith("/")) {
     return (
-      <Link href={href} passHref {...rest}>
+      <Link
+        href={href}
+        passHref
+        className={clsx(styles.anchor, rest.className)}
+        {...rest}
+      >
         {children}
       </Link>
     );
   }
 
-  if (href.startsWith('#')) {
+  if (href.startsWith("#")) {
     return (
-      <a href={href} {...rest}>
+      <a href={href} className={clsx(styles.anchor, rest.className)} {...rest}>
         {children}
       </a>
     );
   }
 
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" {...rest}>
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={clsx(styles.anchor, rest.className)}
+      {...rest}
+    >
       {children}
     </a>
   );

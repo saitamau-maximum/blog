466ab2e0e14fa679171741a67a32779f2b45f562
sor4chi <sorakawa0218@gmail.com>
2023-05-27 23:19:55 +0900

    feat: ブログリレーの一覧ページを作成

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,7 +2,10 @@ import path from "path";
 
 export const URL = {
   BLOG_DIR_PATH: path.join(process.cwd(), "blog"),
+  RELAY_DIR_PATH: path.join(process.cwd(), "relay"),
   BLOG_FILE_PATH: (filename: string) => path.join(URL.BLOG_DIR_PATH, filename),
+  RELAY_FILE_PATH: (filename: string) =>
+    path.join(URL.RELAY_DIR_PATH, filename),
   GITHUB_REPOSITORY_BLOG_URL: (slug: string) =>
     `https://github.com/saitamau-maximum/blog/blob/main/blog/${slug}.md`,
   GITHUB_PROFILE_URL: (username: string) => `https://github.com/${username}`,

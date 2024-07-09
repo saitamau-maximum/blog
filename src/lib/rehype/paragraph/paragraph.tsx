19d021f1b5ad468d540803e45e46b9f602c0d1f8
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-07-09 10:13:36 +0000

    linkcard maybe fix

--- src/lib/rehype/paragraph/paragraph.tsx
+++ src/lib/rehype/paragraph/paragraph.tsx
@@ -3,14 +3,18 @@ import { ReactElement } from 'react';
 import { LinkCard } from '@/components/blog/link-card';
 
 interface Props {
-  children: ReactElement[];
+  children: ReactElement | ReactElement[];
 }
 
 export const Paragraph = ({ children }: Props) => {
-  if (children?.length === 1 && children[0].type === 'a') {
+  if (
+    typeof children === 'object' &&
+    !Array.isArray(children) &&
+    children.type === 'a'
+  ) {
     // もしpタグchildの要素がaタグで、かつ子要素が1つだけの場合
     // (リスト内のリンクの場合は除外)
-    return <LinkCard href={children[0].props.href} />;
+    return <LinkCard href={children.props.href} />;
   }
 
   return <p>{children}</p>;

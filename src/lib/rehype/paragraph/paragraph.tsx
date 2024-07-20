07330b499754b13a5bb0e50a2aa305aa4c35ef80
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-07-20 07:22:37 +0000

    fix

--- src/lib/rehype/paragraph/paragraph.tsx
+++ src/lib/rehype/paragraph/paragraph.tsx
@@ -1,17 +1,13 @@
-import { ReactElement } from 'react';
+import { ReactNode, isValidElement } from 'react';
 
 import { LinkCard } from '@/components/blog/link-card';
 
 interface Props {
-  children: ReactElement | ReactElement[];
+  children: ReactNode;
 }
 
 export const Paragraph = ({ children }: Props) => {
-  if (
-    typeof children === 'object' &&
-    !Array.isArray(children) &&
-    children.type === 'a'
-  ) {
+  if (isValidElement(children) && children.type === 'a') {
     // もしpタグchildの要素がaタグで、かつ子要素が1つだけの場合
     // (リスト内のリンクの場合は除外)
     return <LinkCard href={children.props.href} />;
